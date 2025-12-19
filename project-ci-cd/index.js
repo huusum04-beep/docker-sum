1a34287d206a913215cec24711bf9db04ec1c540
huusum04-beep <huusum04@gmail.com>
2025-12-19 09:19:43 +0700

    Fix nested git and update CI/CD workflow

---
+++ project-ci-cd/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+  res.send('<h1>Xin chao! Day la Website CI/CD cua Toi</h1>');
+});
+
+app.listen(port, () => {
+  console.log(`App dang chay tai port ${port}`);
+});
+console.log("App version 2.0 - CI/CD Triggered");
